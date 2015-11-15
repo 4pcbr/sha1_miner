47cad6af87dee95d661c6b13a446e2cc792f8232
4pcbr <me@4pcbr.com>
2015-11-15 16:46:02 +0100

    GetHash function implemented

--- lib/sha1_miner/cli.ex
+++ lib/sha1_miner/cli.ex
@@ -9,8 +9,8 @@ defmodule Sha1Miner.CLI do
   def parse_args( argv ) do
     case OptionParser.parse(
       argv,
-      switches: [ preffix: :string ],
-      aliases:  [ :px, :preffix ]
+      switches: [ preffix: :string, nodes: :string ],
+      aliases:  [ px: :preffix, n: :nodes ]
     ) do
       { [ preffix: preffix ], _, _ } -> preffix
     end
@@ -19,6 +19,16 @@ defmodule Sha1Miner.CLI do
   def process( preffix ) do
     IO.puts "The preffix is: #{preffix}"
     { num_pref, _ } = Integer.parse( preffix, 0x10 )
+    cur_hash
+  end
+
+  defp cur_hash do
+    case System.cmd( "git", [ "rev-parse", "HEAD" ] ) do
+      { hash, 0 } -> String.strip( hash )
+      { error, error_code } -> 
+        IO.puts error
+        exit error_code
+    end
   end
 
 end
