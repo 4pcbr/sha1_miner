c5ad5a31b53a2f65a993888f565d1437b63d6f60
4pcbr <me@4pcbr.com>
2015-11-17 01:19:41 +0100

    Implemented a draft version of Miner.do_mine function

--- lib/sha1_miner/cli.ex
+++ lib/sha1_miner/cli.ex
@@ -32,34 +32,31 @@ defmodule Sha1Miner.CLI do
     cur_hash
       |> cat_file
       |> parse_commit_dates
-      |> run_miners( num_pref, nodes )
+      |> run_miners( preffix, nodes )
   end
 
-  defp run_miners( { author_date, committer_date }, preffix, nodes ) do
-    IO.puts "the number of nodes: #{nodes}, #{author_date}, #{committer_date}"
+  defp run_miners( { blob, author, committer }, preffix, nodes ) do
+    IO.puts "Running the miners"
     (1..nodes)
       |> Enum.map( fn( _ix ) ->
         IO.puts _ix
-        spawn( Sha1Miner.Miner, :run, [ self, preffix, author_date, committer_date ] )
+        spawn( Sha1Miner.Miner, :run, [ self, blob, preffix, author, committer ] )
       end )
       |> listen_to_miners( [] )
   end
 
   defp listen_to_miners( miners, results ) do
-    IO.puts "listening to the miners"
     receive do
       { :ready, pid } ->
         send( pid, { :calc, Sha1Miner.SequenceServer.next, self } )
         listen_to_miners( miners, results )
-      { :result, { :not_found, _ }, pid } ->
-        send( pid, { :calc, Sha1Miner.SequenceServer.next, self } )
+      { :result, { :not_found, pid } } ->
         listen_to_miners( miners, results )
-      { :result, { :done, the_hash }, pid } ->
+      { :result, { :done, the_hash, pid } } ->
         Enum.each( miners, &(send( &1, :terminate )) )
         [ the_hash | results ]
     end
   end
-
 
   defp cur_hash do
     case System.cmd( "git", [ "rev-parse", "HEAD" ] ) do
@@ -80,9 +77,9 @@ defmodule Sha1Miner.CLI do
   end
 
   defp parse_commit_dates( commit_obj ) do
-    author_date    = Regex.run( ~r/author.+>\s(.+)/m,    commit_obj ) |> Enum.at( 1 ) |> Integer.parse |> elem( 0 )
-    committer_date = Regex.run( ~r/committer.+>\s(.+)/m, commit_obj ) |> Enum.at( 1 ) |> Integer.parse |> elem( 0 )
-    { author_date, committer_date }
+    [ author_date, author_tz ]       = Regex.run( ~r/author.+>\s(.+)/m,    commit_obj ) |> Enum.at( 1 ) |> String.split( " " )
+    [ committer_date, committer_tz ] = Regex.run( ~r/committer.+>\s(.+)/m, commit_obj ) |> Enum.at( 1 ) |> String.split( " " )
+    { commit_obj, { author_date, author_tz }, { committer_date, committer_tz } }
   end
 
 end
--- lib/sha1_miner/miner.ex
+++ lib/sha1_miner/miner.ex
@@ -1,20 +1,35 @@
 defmodule Sha1Miner.Miner do
 
-  def run( scheduler, preffix, author_date, committer_date ) do
-    IO.puts "ololo"
+  def run( scheduler, blob, preffix, author, committer ) do
     send scheduler, { :ready, self }
     receive do
       { :calc, { i, j }, client } ->
-        send client, { :result, do_mine( preffix, author_date, committer_date, i, j ), self }
+        send client, { :result, do_mine( blob, preffix, author, committer, i, j ) }
       { :terminate } ->
         exit( :normal )
     end
+    run( scheduler, blob, preffix, author, committer )
   end
 
-  defp do_mine( preffix, author_date, committer_date, i, j ) do
-    IO.puts "I do mine"
-    #XXX
-    { :done, :blahblah }
+  defp _inspect(v) do
+    IO.inspect v
+    v
+  end
+
+
+  defp do_mine( blob, preffix, { author_date, author_tz }, { committer_date, committer_tz }, i, j ) do
+    { macro_sec, sec, _ } = :erlang.now
+    cur_timestamp = macro_sec + 1_000_000 * sec
+    a_delta = cur_timestamp - i
+    c_delta = cur_timestamp - j
+    case "#{blob}#{a_delta}#{c_delta}"
+            |> :crypto.sha
+            |> :binary.part( { 0, byte_size( preffix ) } ) do
+      k when k == preffix ->
+        { :done, { { a_delta, author_tz }, { c_delta, committer_tz } }, self }
+      default ->
+        { :not_found, self }
+    end
   end
 
 end
